ccbaede1254abe186f631946eed95d6503128bb2
Mans <am00767@gmail.com>
2021-02-26 00:34:06 +0200

    job resource

--- app/Http/Resources/JobResource.php
+++ app/Http/Resources/JobResource.php
@@ -31,6 +31,7 @@ class JobResource extends JsonResource
             'organization_logo'=>$this->organization->img,
             'organization_email'=>$this->organization->user->email,
             'organization_mobile'=>$this->organization->user->mobile,
+            'organization_mobile2'=>$this->organization->user->mobile2,
             'stars' => $this->whenPivotLoaded('volunteers_jobs', function () {
                 return $this->pivot->stars;
             }),
